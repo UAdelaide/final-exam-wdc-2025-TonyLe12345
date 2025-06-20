5deccf02401e295d5911afce5180feebc25a18f0
TONY <a1908861@adelaide.edu.au>
2025-06-20 03:14:31 +0000

    [automatic commit] 2025-06-20 03:14:31 UTC 122.108.52.137

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -6,4 +6,14 @@ router.get('/api/dogs', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/items', async (req, res) => {
+  const [rows] = await db.query(`
+    SELECT bl.BookID, bi.Title, u.Name AS SellerName, bl.SellerID
+    FROM BookListings bl
+    JOIN BookInfo bi ON bl.BookInfoID = bi.BookInfoID
+    JOIN Users u ON bl.SellerID = u.UserID
+  `);
+  res.json(rows);
+});
+
 module.exports = router;

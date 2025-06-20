2190846ee0427b0a2bcc518ab27e7a9e2460d701
TONY <a1908861@adelaide.edu.au>
2025-06-20 05:25:57 +0000

    [automatic commit] 2025-06-20 05:25:57 UTC 122.108.52.137

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-
 router.get('/dogs', async (req, res) => {
   try {
     const [rows] = await db.query(`
@@ -43,11 +42,23 @@ router.get('/walkrequests/open', async (req, res) => {
 });
 
 
-router.get('/walkers/summary', async (req, res) => {
-  const [rows] = await db.query(`
-    SELECT
-  `);
-  res.json(rows);
+router.get('/dogs', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
+        SELECT
+        Dogs.name AS dog_name,
+        Dogs.size,
+        Users.username AS owner_username
+        FROM
+        Dogs
+        JOIN
+        Users ON Dogs.owner_id = Users.user_id;
+    `);
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error Fetching Data' });
+  }
 });
 
 module.exports = router;

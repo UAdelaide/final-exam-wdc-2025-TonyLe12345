44317cfcaa90a0760d6bc63d615c2bc2b55ed6a4
TONY <a1908861@adelaide.edu.au>
2025-06-20 05:29:00 +0000

    [automatic commit] 2025-06-20 05:29:00 UTC 122.108.52.137

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -46,13 +46,22 @@ router.get('/api/walkers/summary', async (req, res) => {
   try {
     const [rows] = await db.query(`
         SELECT
-        Dogs.name AS dog_name,
-        Dogs.size,
-        Users.username AS owner_username
-        FROM
-        Dogs
-        JOIN
-        Users ON Dogs.owner_id = Users.user_id;
+  Users.username AS walker_username,
+  COUNT(DISTINCT WalkRequests.request_id) AS completed_walks,
+  AVG(WalkRatings.rating) AS average_rating
+FROM Users
+LEFT JOIN WalkApplications
+ON Users.user_id = WalkApplications.walker_id
+AND WalkApplications.status = 'accepted'
+LEFT JOIN WalkRequests
+ON WalkApplications.request_id = WalkRequests.request_id
+AND WalkRequests.status = 'completed'
+LEFT JOIN WalkRatings
+ON WalkRequests.request_id = WalkRatings.request_id
+AND WalkRatings.walker_id = Users.user_id
+WHERE Users.role = 'walker'
+GROUP BY Users.username;
+
     `);
     res.json(rows);
   } catch (error) {

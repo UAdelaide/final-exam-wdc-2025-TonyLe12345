33583ea049f46ed912317f5518b11bb2dce30214
TONY <a1908861@adelaide.edu.au>
2025-06-20 13:59:23 +0000

    [automatic commit] 2025-06-20 13:59:23 UTC 122.108.52.137

--- part2/routes/login.js
+++ part2/routes/login.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 

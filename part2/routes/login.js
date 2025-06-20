54b14ac12b6f1f8151eaf50e9b087a7dfed17791
TONY <a1908861@adelaide.edu.au>
2025-06-20 13:34:28 +0000

    [automatic commit] 2025-06-20 13:34:28 UTC 122.108.52.137

--- part2/routes/login.js
+++ part2/routes/login.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const db = require('../models/db');
 
 router.get('/', async (req, res) => {
+  if (!req.session.user) {
+    return res.redirect('/');
+  }
+
   if (req.session.user.role === 'walker') {
     return res.redirect('/api/walks');
   }
 
   if (req.session.user.role === 'owner') {
-    return res.redirect('/api/users');
+    return res.redirect('/workspaces/final-exam-wdc-2025-TonyLe12345/part2/public/owner-dashboard.html');
   }
 });
 

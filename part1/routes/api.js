be84d60b1375995392efd1b058d102bb8be55236
TONY <a1908861@adelaide.edu.au>
2025-06-20 05:24:14 +0000

    [automatic commit] 2025-06-20 05:24:14 UTC 122.108.52.137

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -36,6 +36,26 @@ router.get('/walkrequests/open', async (req, res) => {
   }
 });
 
+router.get('/walkrequests/open', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
+      SELECT
+        Dogs.name AS dog_name,
+        WalkRequests.requested_time,
+        WalkRequests.location,
+        Users.username AS owner_username
+      FROM WalkRequests
+      JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+      JOIN Users ON Dogs.owner_id = Users.user_id
+      WHERE WalkRequests.status = 'open';
+    `);
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error Fetching Data' });
+  }
+});
+
 
 router.get('/walkers/summary', async (req, res) => {
   const [rows] = await db.query(`

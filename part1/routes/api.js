4db9e93855f59a8e8afd3aaf7b364c27fbace57d
TONY <a1908861@adelaide.edu.au>
2025-06-20 06:45:52 +0000

    [automatic commit] 2025-06-20 06:45:52 UTC 122.108.52.137

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -44,7 +44,7 @@ router.get('/walkrequests/open', async (req, res) => {
 });
 
 
-router.get('/api/walkers/summary', async (req, res) => {
+router.get('/walkers/summary', async (req, res) => {
   try {
     const [rows] = await db.query(`
         SELECT

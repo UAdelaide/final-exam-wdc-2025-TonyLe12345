041b4d9846432817d29cf5e2dd9aea69a9353eee
TONY <a1908861@adelaide.edu.au>
2025-06-20 17:16:52 +0000

    [automatic commit] 2025-06-20 17:16:52 UTC 122.108.52.137

--- part2/routes/login.js
+++ part2/routes/login.js
@@ -28,4 +28,23 @@ router.post('/logOut', function (req, res) {
   });
 });
 
+router.get('api/dogs', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
+        SELECT
+        Dogs.name AS dog_name,
+        Dogs.size,
+        Users.username AS owner_username
+        FROM
+        Dogs
+        JOIN
+        Users ON Dogs.owner_id = Users.user_id;
+    `);
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error Fetching Data' });
+  }
+});
+
 module.exports = router;

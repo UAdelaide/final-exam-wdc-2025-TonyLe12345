c2e03b5e3d4f0233af6ae7ef342f001a03324075
TONY <a1908861@adelaide.edu.au>
2025-06-20 06:37:00 +0000

    [automatic commit] 2025-06-20 06:37:00 UTC 122.108.52.137

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -53,8 +53,6 @@ router.get('/api/walkers/summary', async (req, res) => {
         AVG(WalkRatings.rating) AS average_rating,
         COUNT(WalkRequests.request_id) AS completed_walks
         FROM Users
-        LEFT JOIN WalkRatings
-        ON WalkRatings.walker_id = Users.user_id
         LEFT JOIN WalkRequests
         ON WalkRequests.request_id = WalkRatings.request_id AND WalkRequests.status = 'completed'
         LEFT JOIN WalkRatings

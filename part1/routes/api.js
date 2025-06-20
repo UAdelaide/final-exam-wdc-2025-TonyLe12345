42cd66d68cf9aca56f4af465d62f4d18243a2888
TONY <a1908861@adelaide.edu.au>
2025-06-20 03:19:37 +0000

    [automatic commit] 2025-06-20 03:19:37 UTC 122.108.52.137

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -5,14 +5,13 @@ var db = require('../db');
 router.get('/api/dogs', async (req, res) => {
   const [rows] = await db.query(`
     SELECT
-        Dogs.name AS dog_name,
-        Dogs.size,
-        Users.username AS owner_username
+    Dogs.name AS dog_name,
+    Dogs.size,
+    Users.username AS owner_username
     FROM
-        Dogs
+    Dogs
     JOIN
-        Users ON Dogs.owner_id = Users.user_id;
-
+    Users ON Dogs.owner_id = Users.user_id;
   `);
   res.json(rows);
 });

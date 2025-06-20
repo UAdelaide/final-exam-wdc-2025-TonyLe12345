ae8f536865c2d6ad67b2a36f3bc439e9b9547d4f
TONY <a1908861@adelaide.edu.au>
2025-06-20 06:39:03 +0000

    [automatic commit] 2025-06-20 06:39:03 UTC 122.108.52.137

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -56,7 +56,7 @@ router.get('/api/walkers/summary', async (req, res) => {
         LEFT JOIN WalkRequests
         ON Users.user_id = WalkRequests.user_id WHERE WalkRequests.status = 'completed'
         LEFT JOIN WalkRatings
-        ON Walk = Users.user_id
+        ON WalkRequest.request_id = request_id
         WHERE Users.role = 'walker'
         GROUP BY Users.username;
     `);

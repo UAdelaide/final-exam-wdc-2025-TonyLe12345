915030651e448d90e2c8459abb71a6ba7f34e71d
TONY <a1908861@adelaide.edu.au>
2025-06-20 13:33:42 +0000

    [automatic commit] 2025-06-20 13:33:42 UTC 122.108.52.137

--- part2/routes/login.js
+++ part2/routes/login.js
@@ -3,10 +3,6 @@ const router = express.Router();
 const db = require('../models/db');
 
 router.get('/', async (req, res) => {
-  if (!req.session.user) {
-    return res.redirect('/');
-  }
-
   if (req.session.user.role === 'walker') {
     return res.redirect('/api/walks');
   }
